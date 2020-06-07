822c1ed6fb16de813e3dd64a183e8bcd628c79b0
DjangoZorro <e.sertdemiratp@gmail.com>
2020-06-07 15:44:48 +0200

    Progress 8.8

--- src/Controller/NotificationController.php
+++ src/Controller/NotificationController.php
@@ -31,7 +31,7 @@ class NotificationController extends AbstractController
      */
     public function unreadCount()
     {
-        new JsonResponse([
+        return new JsonResponse([
             'count' => $this->notificationRepository->findUnseenByUser($this->getUser())
         ]);
     }
