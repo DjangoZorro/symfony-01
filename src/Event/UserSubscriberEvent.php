0fd9680f8377ba41406d1f8ecdb8b5fdc54200b1
DjangoZorro <e.sertdemiratp@gmail.com>
2020-06-14 14:07:58 +0200

    Progress 11.5

--- src/Event/UserSubscriberEvent.php
+++ src/Event/UserSubscriberEvent.php
@@ -2,7 +2,9 @@
 
 namespace App\Event;
 
+use App\Entity\UserPreferences;
 use App\Mailer\Mailer;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\EventDispatcher\EventSubscriberInterface;
 
 class UserSubscriberEvent implements EventSubscriberInterface
@@ -11,10 +13,20 @@ class UserSubscriberEvent implements EventSubscriberInterface
      * @var Mailer
      */
     private $mailer;
+    /**
+     * @var string
+     */
+    private $defaultLocale;
+    /**
+     * @var EntityManagerInterface
+     */
+    private $entityManager;
 
-    public function __construct(Mailer $mailer)
+    public function __construct(Mailer $mailer, EntityManagerInterface $entityManager, string $defaultLocale)
     {
         $this->mailer = $mailer;
+        $this->defaultLocale = $defaultLocale;
+        $this->entityManager = $entityManager;
     }
 
     public static function getSubscribedEvents()
@@ -26,6 +38,14 @@ class UserSubscriberEvent implements EventSubscriberInterface
 
     public function onUserRegister(UserRegisterEvent $event)
     {
+        $preferences = new UserPreferences();
+        $preferences->setLocale($this->defaultLocale);
+
+        $user = $event->getRegisteredUser();
+        $user->setPreferences($preferences);
+
+        $this->entityManager->flush();
+
         $this->mailer->sendConfirmationEmail($event->getRegisteredUser());
     }
 }
